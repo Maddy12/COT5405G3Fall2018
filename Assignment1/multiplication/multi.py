117073113a4552f161b2ddc52cce1fd3d9966eb5
maddy <maddy.schiappa@hotmail.com>
2018-09-12 12:45:05 -0400

    reduced lines of code for choosing neg versus positive signed integers and added doc strings

--- Assignment1/multiplication/multi.py
+++ Assignment1/multiplication/multi.py
@@ -1,10 +1,18 @@
 import numpy as np
-from random import randint
+from random import randint, choice
 from time import time
 import matplotlib.pyplot as plt
 
 
 def measure_performance(function, rounds=8, n=1000):
+    """
+    This function will measure the performance of a passed function.
+
+    :param function: The function is required to output an avg_runtime and a max_runtime for whatever tasks it is performing.
+    :param rounds: Number of int sizes to iterate through starting at 2**2 = 4
+    :param n: Number of runs per int size.
+    :return:
+    """
     int_len_list = [2**x for x in np.arange(2, rounds+2)]
     avg_runtimes = list()
     max_runtimes = list()
@@ -22,20 +30,21 @@ def measure_performance(function, rounds=8, n=1000):
 
 
 def multiply_no_optimization(int_len, n=1000):
+    """
+    This function multiplys two random signed integers based on a passed integer length.
+    It multiplys two random values 1000 times and records each runtime where then the maximum and average are calculated.
+    :param int_len: Length of integer
+    :param n: Rounds to multiply, new random integers are initialized each run.
+    :return:
+    """
     start_neg = -(10L**int_len)+1
     end_neg = -(10L**(int_len-1))
     start_pos = 10L**(int_len-1)
     end_pos = (10L**int_len)-1  # max value of len int_len
     runtimes = list()
     for i in range(n-1):
-        if randint(1, 2) == 1:
-            x = randint(start_pos, end_pos)
-        else:
-            x = randint(start_neg, end_neg)
-        if randint(1, 2) == 1:
-            y = randint(start_pos, end_pos)
-        else:
-            y = randint(start_neg, end_neg)
+        x = choice([randint(start_neg, end_neg), randint(start_pos, end_pos)])
+        y = choice([randint(start_neg, end_neg), randint(start_pos, end_pos)])
         start_time = time()
         results = x*y
         print(results)
