fixed bug on bigint overflow

Assignment1/multiplication/py3_multi.py
@@ -7,11 +7,11 @@ author = "Madeline Schiappa, Jack P. Oakley, Elakkat Gireesh, Shah Hassan"
 author_email = "madelineschiappa@knights.ucf.edu, jack.p.oakley@knights.ucf.edu, elakkat@knights.ucf.edu, shahhassan@knights.ucf.edu"
 
 
-def measure_performance(function, rounds=8, n=1000):
+def measure_performance(algorithm, rounds=8, n=1000, max_rt=False, log=False):
     """
     This function will measure the performance of a passed function.
 
-    :param function: The function is required to output an avg_runtime and a max_runtime for whatever tasks it is performing.
+    :param algorithm: The algorithm used to run the multiplication task.
     :param rounds: Number of int sizes to iterate through starting at 2**2 = 4
     :param n: Number of runs per int size.
     :return:
@@ -21,11 +21,16 @@ def measure_performance(function, rounds=8, n=1000):
     avg_runtimes = list()
     max_runtimes = list()
     for int_len in int_len_list:
-        avg_runtime, max_runtime = function(int_len, n)
+        avg_runtime, max_runtime = multiply_simulation(int_len, algorithm, n)
         avg_runtimes.append(avg_runtime)
         max_runtimes.append(max_runtime)
+    fig = plt.figure()
+    ax = fig.add_subplot(2, 1, 1)
     plt.plot(int_len_list, avg_runtimes)
-    plt.plot(int_len_list, max_runtimes)
+    if max_rt:
+        plt.plot(int_len_list, max_runtimes)
+    if log:
+        ax.set_yscale("log", nonposy='clip')
     plt.legend(['Average Runtime', 'Max Runtime'], loc='upper left')
     plt.xlabel("Integer Size")
     plt.ylabel("Runtime in microseconds")
@@ -33,34 +38,34 @@ def measure_performance(function, rounds=8, n=1000):
     plt.show()
 
 
-def multiply_no_optimization(int_len, n=1000):
+def multiply_simulation(int_len, algorithm, n=1000):
     """
     This function multiplies two random signed integers based on a passed integer length.
     It multiplies two random values 1000 times and records each runtime where then the maximum and average are calculated.
     :param int_len: Length of integer
+    :param algorithm: algorithm to run with inputs x and y
     :param n: Rounds to multiply, new random integers are initialized each run.
     :return:
     """
-    try:
-        int_len = int(int_len)
-        start_neg = -(10**int_len)+1
-        end_neg = -(10**(int_len-1))
-        start_pos = 10**(int_len-1)
-        end_pos = (10**int_len)-1  # max value of len int_len
-        runtimes = list()
-        for i in range(n-1):
-            x = choice([randint(start_neg, end_neg), randint(start_pos, end_pos)])
-            y = choice([randint(start_neg, end_neg), randint(start_pos, end_pos)])
-            start_time = time()
-            a, b, sign = strip_signs(x, y)
-            results = multiply_by_digit(a, b, sign)
-            end_time = time()
-            print(results)
-            diff = end_time - start_time
-            runtimes.append(diff)
-        return np.mean(runtimes), np.max(runtimes)
-    except:
-        import pdb;pdb.set_trace()
+    int_len = int(int_len)
+    start_neg = -(10**int_len)+1
+    end_neg = -(10**(int_len-1))
+    start_pos = 10**(int_len-1)
+    end_pos = (10**int_len)-1  # max value of len int_len
+    runtimes = list()
+    for i in range(n-1):
+        x = choice([randint(start_neg, end_neg), randint(start_pos, end_pos)])
+        y = choice([randint(start_neg, end_neg), randint(start_pos, end_pos)])
+        start_time = time()
+        results = algorithm(x, y)
+        end_time = time()
+        diff = end_time - start_time
+        runtimes.append(diff)
+    return np.mean(runtimes), np.max(runtimes)
+
+
+def python_optimization(x, y):
+    return x*y
 
 
 def strip_signs(a, b):
@@ -72,51 +77,50 @@ def strip_signs(a, b):
     :return: Absolute value of a, Absolute value of b, Resulting sign from product
     """
     if (a < 0 and b < 0) or (a > 0 and b > 0):
-        res_sign = '-'
+        res_sign = -1
     else:
-        res_sign = '+'
+        res_sign = 1
     a = abs(a)
     b = abs(b)
     return a, b, res_sign
 
 
-def multiply_by_digit(a, b, sign):
+def multiply_by_digit(x, y):
     """
     This function takes two unsigned integers and goes digit by digit to multiply each digit starting from the LSB by
     making use of a carry digit and then adds the resulting multiplications together to get an end result of the product
     of the two integers.  It then attaches the passed in sign to return a properly signed product.
-    :param a: First integer to be multiplied
-    :param b: Second integer to be multiplied
-    :param sign: Sign of the end product
+    :param x: First integer to be multiplied
+    :param y: Second integer to be multiplied
     :return: Product of a and b with proper sign
     """
-    sum_result = x = 0
+    a, b, sign = strip_signs(x, y)
+    x = sum_result = 0
+    y = 0
     while b > 0:
         carry = y = res2sum = shift_y = 0
         a_temp = a
         shift_x = 10**x
-        b_lsb = int(b % 10)
+        b_lsb = b % 10
         while a_temp > 0:
             shift_y = 10**y
-            a_lsb = int(a_temp % 10)
+            a_lsb = a_temp % 10
             result = a_lsb * b_lsb + carry
             if result > 9:
-                carry = int(result / 10)
+                carry = result // 10
                 result %= 10
             else:
                 carry = 0
-            a_temp = int(a_temp / 10)
-            # Prev line crashes when number has 512+ digits
+            a_temp = a_temp // 10
             res2sum += result * shift_y
             y += 1
         res2sum += carry * shift_y * 10
         sum_result += res2sum * shift_x
-        b = int(b / 10)
+        b = b // 10
         x += 1
-    sum_result = int(sign + str(sum_result))
-    # print(sum_result)
+    sum_result = sign * sum_result
     return sum_result
 
 
 if __name__ == '__main__':
-    measure_performance(function=multiply_no_optimization)
+    measure_performance(algorithm=multiply_by_digit, log=True)